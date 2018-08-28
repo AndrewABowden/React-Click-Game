99422b111361d45ef0a2af44f4e08b7bcb5cc961
Andrew <andrewbowden92@gmail.com>
2018-08-28 11:18:50 -0700

    v6

--- react-clicker/app.js
+++ react-clicker/app.js
@@ -1,115 +1,109 @@
-
-import React, { Component } from "react";
+import React, {Component} from "react";
 import { Line } from 'rc-progress';
-import Wrapper from "./components/Wrapper";
+import Wrapper from ".components/Wrapper";
 import Card from './components/Card';
-import Characters from './characters.json';
-import './App.css';
+import Heroes from "./Heroes.json";
+import "./style.css"
+// import API from "../../utils/API";
 
-let topScore = 0;
-let guessesCorrect = 0;
-let hearts = 0;
-let message = "";
+class App extends React.Component {
+    state = {
+        Heroes,
+        topScore,
+        guessesCorrect,
+        message,
+        lives
+    }
+    // handeIncrement() {
+    //     this.setState({ count: this.state.count})
+    // }
 
-class App extends Component {
+    setClicked = id => {
+        //Set heroes state to default variable
+        const Heroes = this.state.Heroes;
+        //filter through Heroes obj cards
+        const cardClicked = Heroes.filter(Hero => Hero.id === id);
 
-	state = {
-		Characters,
-		topScore,
-		guessesCorrect,
-		message, 
-		hearts
-	};
+        //if card clicked run score functions
+        if(cardClicked[0].clicked) {
+            guessesCorrect = 0;
+            message = "Wrong. try again!";
 
-	setClicked = id => {
-		const Characters = this.state.Characters;
-		const cardClicked = Characters.filter(Character => Character.id === id);
+            //change to map
+                for(let i = 0; i < Heroes.length; i++) {
+                    Heroes[i].clicked = false;
+                }
+        this.setState({message});
+        this.setState({guessesCorrect});
+        this.setState({Heroes});
+            } else {
+    cardClicked[0].clicked = true;
+    //change guesses if right and add extra guesses
+    guessesCorrect = guessesCorrect + 2;
+    message = "You were right!"
 
-		if (cardClicked[0].clicked) {
-
-			guessesCorrect = 0;
-			message = 'Whoops. Start over';
-
-			// change to map or smth?
-			for (let i = 0; i < Characters.length; i++) {
-				Characters[i].clicked = false;
-			}
-
-			this.setState({message});
-			this.setState({guessesCorrect});
-			this.setState({Characters});
-
-		} else {
-			cardClicked[0].clicked = true;
-
-			guessesCorrect = guessesCorrect + 4;
-			message = "Good Job!"
-
-			if (guessesCorrect > topScore) {
-				topScore = guessesCorrect;
-				hearts++;
-				this.setState({hearts});
-				this.setState({topScore});
-				this.renderHearts();
-			}
-
-			Characters.sort((a, b) => {
-				return 0.5 - Math.random();
-			});
-
-			this.setState({Characters});
-			this.setState({guessesCorrect});
-			this.setState({message});
-		}
-	};
-
-	renderHearts() {
-		let divs = [];
-
-		for (let i = 0; i < this.state.hearts; i++) {
-			divs.push(<div key={i} className="heart"></div>);
-		}
-
-		return <div>{divs}</div>;
-	};
-
-    render() {
-
-        return ( 
-        	<Wrapper>
-    			<div className="hero">
-    				<div className="heroText">
-    					<h1 className="banner">The Legend of Zelda Click Game</h1>
-        				<h3 className="rules">Save Hyrule! Choose wisely.</h3>
-        				<h3 className="message">{this.state.message}</h3>
-    				</div>
-    				<div className="buttonWrapper">
-    					<img className="buttons" src="images/buttons.png" alt="zelda buttons" />
-    				</div>
-					<div className="heartWrapper">
-    				{this.renderHearts()}
-    				<Line 
-						className="progress-bar"
-	        			percent={this.state.guessesCorrect}
-	        			trailWidth="8" 
-	        			strokeWidth="8" 
-	        			strokeColor="#87df6f"
-						strokeLinecap="square" />
-    				</div>
-    			</div>
-            	<div className="row">
-            		{this.state.Characters.map(Character => (
-            			<Card
-            				setClicked={this.setClicked}
-            				id={Character.id}
-            				key={Character.id}
-            				image={Character.image}
-            				name={Character.name}
-            				className="col-sm-1"
-            			/>
-            		))}
-            	</div>
-            </Wrapper>
-        );
+        if(guessesCorrect > topScore) {
+            topScore = guessesCorrect;
+            lives++;
+            this.setState({lives});
+            this.setState({topScore});
+            this.renderLives();
+        }
+        Heroes.sort((a,b) =? {
+            return 0.5 - Math.random();
+        });
+        this.setState({Heroes}));
+        this.setState({guessesCorrect});
+        this.setState({message});
     }
 };
+
+renderLives () {
+    let divs = [];
+
+    for (let i = 0; i < this.state.lives; i++) {
+        divs.push(<div key={i} className="life"></div>);
+    }
+
+    return <div>{divs}</div>;
+};
+
+render() {
+    return (
+        <Wrapper>
+            <div className="character">
+                <div className="characterText">
+                    <h1 className="banner">The Lord of The Rings Click Game</h1>
+                    <h3 className="rules">Save Middle Earth from Sauron!</h3>
+                    <h3 className="message">{this.state.message}</h3>
+                </div>
+                <div className="buttonWrapper">
+                    <img className="buttons" src="images/buttons.png" alt="lotr buttons" />
+                </div>
+                    <div className="lifeWrapper">
+                    {this.renderLives()}
+                    <Line
+                        classname="progress-bar"
+                        percent={this.state.guessesCorrect}
+                        trailWidth="4"
+                        strokWidth="5"
+                        strokeColor="red"
+                            strokeLinecap="square" />
+                    </div>
+            </div>
+            <div className="row">
+                {this.state.Heroes.map(Hero => (
+                    <Card   
+                        setClicked={this.setClicked}
+                        id={Hero.id}
+                        key={Hero.id}
+                        image={Hero.image}
+                        name={Hero.name}
+                        className="col-sm-1" />
+            </div>
+        </Wrapper>
+    );
+}
+}
+}
+export default App;
